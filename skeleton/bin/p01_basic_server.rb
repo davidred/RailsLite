1d70d772ebfeb2f4d7fcbe997394948d41e1c3d1
David Rozenberg <daveroze@gmail.com>
2014-10-11 11:44:45 -0400

    Create basic server

--- skeleton/bin/p01_basic_server.rb
+++ skeleton/bin/p01_basic_server.rb
@@ -1,5 +1,15 @@
 require 'webrick'
 
+server = WEBrick::HTTPServer.new Port: 3000
+
+server.mount_proc '/' do |req, res|
+  res.body = "Hello World!"
+end
+
+server.start
+
+trap 'INT' do server.shutdown end
+
 # http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick.html
 # http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/HTTPRequest.html
 # http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/HTTPResponse.html
