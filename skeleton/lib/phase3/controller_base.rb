1318cb6e13620d4590524c4cc92dc0f123ddc44a
David Rozenberg <daveroze@gmail.com>
2014-10-11 16:45:17 -0400

    add template rendering

--- skeleton/lib/phase3/controller_base.rb
+++ skeleton/lib/phase3/controller_base.rb
@@ -1,5 +1,6 @@
 require_relative '../phase2/controller_base'
 require 'active_support/core_ext'
+require 'active_support/inflector'
 require 'erb'
 
 module Phase3
@@ -7,6 +8,12 @@ module Phase3
     # use ERB and binding to evaluate templates
     # pass the rendered html to render_content
     def render(template_name)
+      filepath = "views/#{self.class.to_s.underscore}/#{template_name}.html.erb"
+      contents = File.read(filepath)
+      template = ERB.new(contents)
+      rendered_html = template.result(binding)
+      
+      render_content(rendered_html, "text/html")
     end
   end
 end
