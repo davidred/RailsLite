fae3e5be7b4fd7f42941378077f5ba95e410de18
David Rozenberg <daveroze@gmail.com>
2014-10-11 13:00:40 -0400

    basic controller redirect and render responses

--- skeleton/lib/phase2/controller_base.rb
+++ skeleton/lib/phase2/controller_base.rb
@@ -4,20 +4,39 @@ module Phase2
 
     # Setup the controller
     def initialize(req, res)
+      @req = req
+      @res = res
+      
+      # puts req
     end
 
     # Helper method to alias @already_built_response
     def already_built_response?
+      @already_built_response ? true : false
     end
 
     # Set the response status code and header
     def redirect_to(url)
+      raise "Already tried to render" if already_built_response?
+
+      @res.status = 302
+      @res["Location"] = url
+
+      @already_built_response = res
+
     end
 
     # Populate the response with content.
     # Set the response's content type to the given type.
     # Raise an error if the developer tries to double render.
     def render_content(content, type)
+      raise "Already tried to render" if already_built_response?
+
+      @res.body = content
+      @res.content_type = type
+    
+      @already_built_response = res
+
     end
   end
 end
