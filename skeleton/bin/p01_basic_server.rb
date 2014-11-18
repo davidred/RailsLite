c60c3c7f25cf975c16ee9281e143388060e48135
David Rozenberg <daveroze@gmail.com>
2014-11-18 09:48:01 -0500

    changed file structure

--- skeleton/bin/p01_basic_server.rb
+++ skeleton/bin/p01_basic_server.rb
@@ -9,8 +9,3 @@ end
 server.start
 
 trap 'INT' do server.shutdown end
-
-# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick.html
-# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/HTTPRequest.html
-# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/HTTPResponse.html
-# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/Cookie.html
